bc90c1912952117f0be2eb65e9cd2e81437b19f9
kimjaehoug <150125391+kimjaehoug@users.noreply.github.com>
2025-03-03 00:46:23 +0900

    first commitfff

--- projectreservation/settings.py
+++ projectreservation/settings.py
@@ -53,11 +53,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SESSION_COOKIE_SECURE = False  # 로컬 개발에서는 False (HTTPS가 아닌 경우)
+SESSION_COOKIE_SECURE = True  # 로컬 개발에서는 False (HTTPS가 아닌 경우)
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SAMESITE = 'Lax'  # CORS와 함께 사용
 
-CORS_ALLOW_CREDENTIALS = False  # 쿠키 및 인증 정보 허용
+CORS_ALLOW_CREDENTIALS = True  # 쿠키 및 인증 정보 허용
 
 ROOT_URLCONF = 'projectreservation.urls'
 
